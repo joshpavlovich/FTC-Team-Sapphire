c545653d186e4650c43f13387259562c1e3c99b6
Josh Pavlovich <joshpavlovich@gmail.com>
2025-05-31 08:26:10 -0500

    Refactor: Update arm motor constants for new motor
    - Update `INTAKE_ARM_MOTOR_TICKS_PER_MM` for 117 RPM motor.
    - Adjust `INTAKE_ARM_PICKUP_POSITION` slightly.

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/robot/ArmState.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/robot/ArmState.kt
@@ -1,12 +1,11 @@
 package org.firstinspires.ftc.teamcode.robot
 
-// Encoder Resolution for Arm 84 RPM Motor = ((((1+(46/17))) * (1+(46/17))) * (1+(46/11)) * 28)
-// From https://www.gobilda.com/5203-series-yellow-jacket-planetary-gear-motor-71-2-1-ratio-24mm-length-8mm-rex-shaft-84-rpm-3-3-5v-encoder/
+// From https://www.gobilda.com/5203-series-yellow-jacket-planetary-gear-motor-50-9-1-ratio-24mm-length-8mm-rex-shaft-117-rpm-3-3-5v-encoder/
 // Ticks Per Revolution = (Motor Encoder Resolution / Diameter Millimeters)
-// Encoder Resolution Formula -> ((((1+(46/17))) * (1+(46/17))) * (1+(46/11)) * 28) = 1992.6
-private const val INTAKE_ARM_MOTOR_TICKS_PER_MM = (1992.6) / 96.0
+// Encoder Resolution Formula	((((1+(46/17))) * (1+(46/17))) * (1+(46/17)) * 28) = 1,425.1
+private const val INTAKE_ARM_MOTOR_TICKS_PER_MM = (1425.1) / 96.0
 private const val INTAKE_ARM_TRANSFER_POSITION = 2.3 * INTAKE_ARM_MOTOR_TICKS_PER_MM
-private const val INTAKE_ARM_PICKUP_POSITION = 47.4 * INTAKE_ARM_MOTOR_TICKS_PER_MM
+private const val INTAKE_ARM_PICKUP_POSITION = 47.3 * INTAKE_ARM_MOTOR_TICKS_PER_MM
 private const val INTAKE_ARM_LOW_CHAMBER_SCORING_POSITION = 32 * INTAKE_ARM_MOTOR_TICKS_PER_MM
 
 private const val RANGE_VARIANCE = 5
