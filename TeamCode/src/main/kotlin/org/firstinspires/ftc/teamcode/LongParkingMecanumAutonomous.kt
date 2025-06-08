c821ac6cf861f9dcc07477da767abc7dfe484ad7
Josh Pavlovich <joshpavlovich@gmail.com>
2025-06-08 11:15:59 -0500

    Disable old parking only autos

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/LongParkingMecanumAutonomous.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/LongParkingMecanumAutonomous.kt
@@ -1,11 +1,13 @@
 package org.firstinspires.ftc.teamcode
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.qualcomm.robotcore.eventloop.opmode.Disabled
 
 private const val DRIVE_SPEED: Double = 0.4
 
 private const val SECONDS_TO_OBSERVATION_ZONE_FROM_START: Double = 1.65
 
+@Disabled
 @Autonomous(name = "LONG Autonomous", group = "Robot")
 open class LongParkingMecanumAutonomous : ParkingMecanumAutonomous() {
 
@@ -20,6 +22,7 @@ open class LongParkingMecanumAutonomous : ParkingMecanumAutonomous() {
     override fun getSecondsToDelayFromStart(): Double = 0.0
 }
 
+@Disabled
 @Autonomous(name = "DELAYED LONG Autonomous", group = "Robot")
 class DelayedLongParkingMecanumAutonomous : LongParkingMecanumAutonomous() {
     override fun getSecondsToDelayFromStart(): Double = DELAY_SECONDS_PARKING_TO_OBSERVATION_ZONE
--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/ShortParkingMecanumAutonomous.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/ShortParkingMecanumAutonomous.kt
@@ -1,11 +1,13 @@
 package org.firstinspires.ftc.teamcode
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.qualcomm.robotcore.eventloop.opmode.Disabled
 
 private const val DRIVE_SPEED: Double = 0.4
 
 private const val SECONDS_TO_OBSERVATION_ZONE_FROM_START: Double = 1.0
 
+@Disabled
 @Autonomous(name = "SHORT Autonomous", group = "Robot")
 open class ShortParkingMecanumAutonomous : ParkingMecanumAutonomous() {
 
@@ -20,6 +22,7 @@ open class ShortParkingMecanumAutonomous : ParkingMecanumAutonomous() {
     override fun getSecondsToDelayFromStart(): Double = 0.0
 }
 
+@Disabled
 @Autonomous(name = "DELAYED SHORT Autonomous", group = "Robot")
 class DelayedParkingMecanumAutonomous : ShortParkingMecanumAutonomous() {
     override fun getSecondsToDelayFromStart(): Double = DELAY_SECONDS_PARKING_TO_OBSERVATION_ZONE
