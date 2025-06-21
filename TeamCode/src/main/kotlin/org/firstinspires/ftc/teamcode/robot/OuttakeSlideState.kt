cdabc1ab4e10cd78dec39f34a71605dfc87fa040
Josh Pavlovich <joshpavlovich@gmail.com>
2025-06-20 20:17:17 -0500

    Update outtake slide motor encoder resolution
    The encoder resolution for the outtake slide motor has been updated to reflect the change to a 435 RPM GoBilda Yellow Jacket motor (previously 223 RPM).
    
    The `SLIDE_LIFT_TICKS_PER_MM` constant has been adjusted accordingly from `751.8 / 120` to `384.54 / 120`.

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/robot/OuttakeSlideState.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/robot/OuttakeSlideState.kt
@@ -1,10 +1,10 @@
 package org.firstinspires.ftc.teamcode.robot
 
-// Encoder Resolution for Viper Slide 223 RPM Motor = ((((1+(46/11))) * (1+(46/11))) * 28)
-// From https://www.gobilda.com/5203-series-yellow-jacket-planetary-gear-motor-26-9-1-ratio-24mm-length-8mm-rex-shaft-223-rpm-3-3-5v-encoder/
+// Encoder Resolution for Viper Slide 435 RPM Motor = ((((1+(46/17))) * (1+(46/17))) * 28)
+// From https://www.gobilda.com/5203-series-yellow-jacket-planetary-gear-motor-13-7-1-ratio-24mm-length-8mm-rex-shaft-435-rpm-3-3-5v-encoder/
 // Ticks Per Revolution = (Motor Encoder Resolution / Diameter Millimeters)
-// Encoder Resolution Formula -> ((((1+(46/11))) * (1+(46/11))) * 28) = 751.8
-private const val SLIDE_LIFT_TICKS_PER_MM = (751.8) / 120
+// Encoder Resolution Formula -> ((((1+(46/17))) * (1+(46/17))) * 28) = 384.54
+private const val SLIDE_LIFT_TICKS_PER_MM = (384.54) / 120
 private const val SLIDE_LIFT_COLLAPSED = 0.0
 private const val SLIDE_LIFT_LEVEL_ONE_ASCENT = 215.9 * SLIDE_LIFT_TICKS_PER_MM
 // Distance in Millimeters for High Basket scoring position = high basket height in Millimeters * Viper Slide Lift Ticks Per Millimeter
